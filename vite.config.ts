3b81c089168560217016ad3b364079cbd9fb5197
primehouseexperts <primehouseexperts@gmail.com>
2026-02-03 21:49:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig(({ mode }) => {
   const geminiKey = env.GEMINI_API_KEY ?? '';
 
   return {
-    base: mode === 'production' ? `/${repoName}/` : '/',
+    base: mode === 'production' ? '/prime-house-builders/' : '/',
+
     server: { port: 3000, host: '0.0.0.0' },
     plugins: [react()],
     define: {
